ee427c68e2019ce1c1ce8a5e71258b01031da122
Andy Costello <andrew.costello@metrisk.com>
2020-06-08 09:46:59 +0100

    Allow button to be a regular submit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -39,13 +39,19 @@ const Button = ({ className, href, type, icon, submit, disabled, children, onCli
   const Tag = href ? RouterLink : 'button'
   const btnType = submit ? 'submit' : !href ? 'button' : undefined
 
+  const handleClick = (event: React.SyntheticEvent) => {
+    if (onClick) {
+      onClick(event)
+    }
+  }
+
   return (
     <Tag
       className={cx(className, 'btn', types[type], alignments[icon?.align], { 'btn--disabled': disabled })}
       type={btnType}
       to={href}
       disabled={disabled}
-      onClick={onClick}
+      onClick={handleClick}
     >
       {icon?.align === 'Left' && <Icon className={'btn__icn'} {...icon} />}
       {icon?.align === 'Center' && <Icon className={'btn__icn'} {...icon} />}
