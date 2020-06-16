904ff2a78da49dbd47d075c1dec34fcc8f08749f
Andy Costello <andrew.costello@metrisk.com>
2020-06-16 16:48:33 +0100

    Build

--- dist/components/Image/Image.d.ts
+++ dist/components/Image/Image.d.ts
@@ -4,8 +4,5 @@ import IImage from './types';
  * Styles
  */
 import './Image.scss';
-/**
- * My component
- */
 declare const Image: ({ className, type, aspect, src, alt, fallback, background }: IImage.IProps) => JSX.Element;
 export default Image;
